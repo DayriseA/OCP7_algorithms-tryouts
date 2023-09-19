d2ee5789447d4887c07298169deb1d5d113e21da
DayriseA_dom <adrien.maisak@gmail.com>
2023-09-19 10:36:46 +0200

    feat: add an alternative to bruteforce without using itertools. add a timer decorator
    function 'generate_combinations' used by 'bruteforce_wo_itertools'

--- bruteforce.py
+++ bruteforce.py
@@ -7,6 +7,7 @@ really horrible to write, and O(20^20) seems like a pretty bad idea to try.
 
 import csv
 import itertools
+import time
 
 
 class Bond:
@@ -17,6 +18,20 @@ class Bond:
         self.profit = price * yield_ / 100
 
 
+def timer(func):
+    """Decorator to measure the execution time of a function."""
+
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"Function '{func.__name__}' took {end - start:.2f} seconds to execute.")
+        return result
+
+    return wrapper
+
+
+@timer
 def bruteforce_w_itertools(bonds, funds):
     """
     Takes a list of bonds and amount of funds available. A bond can be bought only once.
@@ -39,6 +54,55 @@ def bruteforce_w_itertools(bonds, funds):
     return best_combination, best_profit, best_combination_price
 
 
+def generate_combinations(bonds):
+    """
+    Takes a list and returns all possible combinations of its elements.
+    Combinations are combined with total price and profit in the form of a tuple.
+    Not using itertools but bitmasking instead.
+    """
+    # Will be used for the solution without itertools
+    n = len(bonds)
+    combinations = []
+
+    for i in range(1, 2**n):
+        current_combination = []
+        total_price = 0
+        total_profit = 0
+
+        for j in range(n):
+            if (i >> j) & 1:
+                bond = bonds[j]
+                current_combination.append(bond)
+                total_price += bond.price
+                total_profit += bond.profit
+
+        combinations.append((current_combination, total_price, total_profit))
+
+    return combinations
+
+
+@timer
+def bruteforce_wo_itertools(bonds, funds):
+    """
+    Takes a list of bonds and amount of funds available. A bond can be bought only once.
+    Returns the optimal bonds combination and profit.
+    Not using itertools.
+    """
+    best_combination = []
+    best_profit = 0
+    best_combination_price = 0
+
+    combinations = generate_combinations(bonds)
+
+    for combination, total_price, total_profit in combinations:
+        if total_price <= funds and total_profit > best_profit:
+            best_combination = combination
+            best_profit = total_profit
+            best_combination_price = total_price
+
+    return best_combination, best_profit, best_combination_price
+
+
 def main():
     bonds = []
     funds = 500
@@ -51,11 +115,12 @@ def main():
             bond = Bond(row["Bonds"], int(row["Price"]), int(row["Yield"]))
             bonds.append(bond)
 
-    # for bond in bonds:
-    #     print(f"{bond.name} - {bond.price} - {bond.yield_} - {bond.profit}")
-
     best_combination, best_profit, total_price = bruteforce_w_itertools(bonds, funds)
+    print("Best combination: ", [bond.name for bond in best_combination])
+    print(f"Associated profit: {best_profit:.2f} €")
+    print(f"Total price: {total_price} €\n")
 
+    best_combination, best_profit, total_price = bruteforce_wo_itertools(bonds, funds)
     print("Best combination: ", [bond.name for bond in best_combination])
     print(f"Associated profit: {best_profit:.2f} €")
     print(f"Total price: {total_price} €")
